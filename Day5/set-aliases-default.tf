b824ce7e2d96db52e5a2d2b6bbc5ee39f7a030c7
ManibhSain <113257421+Manibh1@users.noreply.github.com>
2024-01-28 13:38:32 +0530

    Create set-aliases-default.tf

---
+++ Day5/set-aliases-default.tf
@@ -0,0 +1,12 @@
+# The default provider configuration; resources that begin with `aws_` will use
+# it as the default, and it can be referenced as `aws`.
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Additional provider configuration for west coast region; resources can
+# reference this as `aws.west`.
+provider "aws" {
+  alias  = "west"
+  region = "us-west-2"
+}
