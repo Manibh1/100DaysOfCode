6d06a9876418f1c09e707457539675fce51741e8
ManibhSain <113257421+Manibh1@users.noreply.github.com>
2024-01-29 22:33:01 +0530

    Create Variables.tf

---
+++ Day6/Variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS region in which the S3 bucket will be created"
+}
