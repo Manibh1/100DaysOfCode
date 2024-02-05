ec95180f1537ec3d7b31fd28592d8e73daed8632
ManibhSain <113257421+Manibh1@users.noreply.github.com>
2024-02-05 23:23:35 +0530

    Create Day12.tf

---
+++ Day12.tf
@@ -0,0 +1,11 @@
+# Declare an AWS EC2 instance resource
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+# Addressing the resource
+aws_instance.example
